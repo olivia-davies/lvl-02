8815c221eebb8adfbeff5e71da85d9b70f4484fe
Olivia Davies <olivia.davies@eagles.ewu.edu>
2017-04-23 20:09:57 -0700

    GIANT update
    *basically finished the tutorial without committing haha

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-// @codekit-prepend "jquery.js";
-// @codekit-prepend "semantic.js";
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
-console.log('Hello, World!');
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
+    const min = now.getMinutes();
+    const minDegrees = ((min / 60) * 360 + ((seconds/60)*6)) + 90;
+    minHand.style.transform = `rotate(${minDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = (((hour / 12) * 360) + ((min/60)*30)) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+}
+
+setInterval(setDate, 1000);
+
+setDate();
+
+
